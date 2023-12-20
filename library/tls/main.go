0d4758c2d59a4d05510f5bf8347b611e1b84ff43
Evan Bee <evan2884@gmail.com>
2023-12-20 11:06:25 +0800

    Library > tls

---
+++ library/tls/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	seedUrl := "https://www.yueplan.top"
+	resp, err := client.Get(seedUrl)
+	defer resp.Body.Close()
+
+	if err != nil {
+		fmt.Errorf(seedUrl, " 请求失败")
+		panic(err)
+	}
+
+	//fmt.Println(resp.TLS.PeerCertificates[0])
+	certInfo := resp.TLS.PeerCertificates[0]
+	fmt.Println("过期时间:", certInfo.NotAfter)
+	fmt.Println("组织信息:", certInfo.Subject)
+}
